252548660d8aa39442d5382315d5c239c40454c5
Sam Gold <sgold5757@gmail.com>
2020-11-27 19:51:06 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -11,8 +11,15 @@ def apply_coupons(cart, coupons)
     if item4markdown
       item4markdown[:count] = item4markdown[:count] - coupon_item[:num]
       binding.pry
+      cart.push {
+        :item => "#{item4markdown[:item]} W/ COUPON",
+        :price => (coupon_item[:cost] / coupon_item[:num]).round(2),
+        :clearance => item4markdown[:clearance],
+        :count => coupon_item[:num]
+      }
     end
   end
+  cart
 end
 
 def apply_clearance(cart)
